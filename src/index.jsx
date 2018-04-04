7efa4722f634de09aab75a8021f00dbef44f317c
ririko <ririkokanikama25@yahoo.co.jp>
2018-04-05 01:09:50 +0900

    [add] babel loader

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const Hello = () => {
+    return <div>hello react and babel and webpack!!!</div>;
+}
+
+ReactDOM.render(
+    <Hello />,
+    document.body
+);
